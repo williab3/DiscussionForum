7cfb3f068ec96055a7b5f8fb143edefe9b36b0b9
Ben W <mrwilliams20602@hotmail.com>
2019-06-19 05:51:18 -0400

    Add project files.

---
+++ DiscussionForum/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DiscussionForum.Startup))]
+namespace DiscussionForum
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
